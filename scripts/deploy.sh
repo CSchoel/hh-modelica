49d01ea9049c62cda7b9eb4ae48d76674d51efb0
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2019-11-22 11:04:33 +0100

    adds documentation and quotes to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+# copies current version of modelica files to deploy directory
+# must be run from within this folder
+
 deploy=~/Documents/Promotion/code/modelica-deploy/HH-modelica
 if [ -d "$deploy" ]; then
-  echo Removing $deploy ...
+  echo "Removing $deploy ..."
   rm -rf "$deploy"
 fi
-echo Copying files from ../HHmodelica to $deploy
+echo "Copying files from ../HHmodelica to $deploy"
 mkdir "$deploy"
 cp -R ../HHmodelica "$deploy/"
+
