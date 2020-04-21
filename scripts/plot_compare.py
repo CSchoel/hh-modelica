dca8d8c9bd2f85463245f66d356609f20080f4a6
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-04-21 16:03:34 +0200

    adds plotting script

---
+++ scripts/plot_compare.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+def compare(afile, bfile):
+    adata, bdata = [pd.read_csv(x, delimiter=",") for x in [afile, bfile]]
+    f = plt.figure(figsize=(6,2))
+    ax = f.add_subplot()
+    ax.plot(adata["time"] * 1000, adata["currentClamp.V"], label="modular")
+    ax.plot(bdata["time"], bdata["V"], "--", label="monolithic")
+    ax.set_xlabel("time [ms]")
+    ax.set_ylabel("voltage [mV]")
+    ax.legend(loc="best")
+    f.tight_layout()
+    if not os.path.exists("plots"):
+        os.mkdir("plots")
+    f.savefig("plots/modular_vs_monolithic.pdf")
+    f.savefig("plots/modular_vs_monolithic.png")
+
+if __name__ == "__main__":
+    compare(
+        "out/HHmodelica.CompleteModels.HHmodFlat_res.csv",
+        "out/HHmodelica.CompleteModels.HHmono_res.csv"
+    )
